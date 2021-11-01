00a4e9ed9ccc7c1dfe9e10791c04dd30c4f0c0ce
unknown <agirgin@sabanciuniv.edu>
2021-11-01 19:38:42 +0300

    'firstLoginPage'

--- SportsCollaborationApp/urls.py
+++ SportsCollaborationApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.mainPage, name="main")
+  path('', views.mainPage, name="main"),
+  path('login',views.LoginPage, name="login"),
 ]
--- SportsCollaborationApp/views.py
+++ SportsCollaborationApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def mainPage(request):
   return render(request, 'pages/main.html')
+
+def LoginPage(request):
+  return render(request, "pages/login.html")

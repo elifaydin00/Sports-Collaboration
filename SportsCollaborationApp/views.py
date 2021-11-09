649c76ae68d367235e0b0ebc821ed87032fcb699
Atakan Çelikyürek <acelikyurek@sabanciuniv.edu>
2021-11-09 20:27:44 +0300

    Database is changed to mysql

--- SportsCollaborationApp/views.py
+++ SportsCollaborationApp/views.py
@@ -7,93 +7,93 @@ from .models import *
 
 def loginPage(request):
 
-  if request.user.is_authenticated:
-    return redirect('main')
+    if request.user.is_authenticated:
+        return redirect('main')
 
-  if request.method == 'POST':
+    if request.method == 'POST':
 
-    email = request.POST['email']
-    password = request.POST['password']
+        email = request.POST['email']
+        password = request.POST['password']
 
-    try:
-      user = SiteUser.objects.get(email=email)
-    except:
-      messages.error(request, 'User does not exist!')
-      return redirect('login')
+        try:
+            user = SiteUser.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist!')
+            return redirect('login')
 
-    user = auth.authenticate(request, username=email.split("@")[0], password=password)
+        user = auth.authenticate(request, username=email.split("@")[0], password=password)
 
-    if user is not None:
-      auth.login(request, user)
-      return redirect('main')
-    else:
-      messages.error(request, 'Email or password is incorrect!')
-      return redirect('login')
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, 'Email or password is incorrect!')
+            return redirect('login')
 
-  return render(request, "pages/LoginPage.html")
+    return render(request, "pages/LoginPage.html")
 
 def registerPage(request):
 
-  if request.user.is_authenticated:
-    return redirect('main')
+    if request.user.is_authenticated:
+        return redirect('main')
 
-  if request.method == 'POST':
+    if request.method == 'POST':
 
-    name = request.POST['fullname']
-    email = request.POST['email']
-    age = request.POST['age']
-    number = request.POST['number']
-    gender = request.POST['gender']
-    location = request.POST['location']
-    password = request.POST['password']
-    password2 = request.POST['password2']
+        name = request.POST['fullname']
+        email = request.POST['email']
+        age = request.POST['age']
+        number = request.POST['number']
+        gender = request.POST['gender']
+        location = request.POST['location']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-    if (name == "" or gender == "Please select" or password == "" or number == "" or email == "" or password2 == "" or age == "" or location == ""):
-      messages.error(request, 'Please, fill all of fields!')
-      return redirect('register')
+        if (name == "" or gender == "Please select" or password == "" or number == "" or email == "" or password2 == "" or age == "" or location == ""):
+            messages.error(request, 'Please, fill all of fields!')
+            return redirect('register')
 
-    try:
-      user = SiteUser.objects.get(email=email)
-      messages.error(request, 'Email is already registered!')
-      return redirect('register')
-    except:
-      pass
+        try:
+            user = SiteUser.objects.get(email=email)
+            messages.error(request, 'Email is already registered!')
+            return redirect('register')
+        except:
+            pass
 
-    if int(age) < 15 or int(age) > 55:
-      messages.error(request, 'Your age is not eligible to sign up!')
-      return redirect('register')
+        if int(age) < 15 or int(age) > 55:
+            messages.error(request, 'Your age is not eligible to sign up!')
+            return redirect('register')
 
-    try:
-      user = SiteUser.objects.get(phoneNumber=number)
-      messages.error(request, 'Phone number is already registered!')
-      return redirect('register')
-    except:
-      pass
-    
-    if password != password2:
-      messages.error(request, 'Passwords do not match!')
-      return redirect('register')
+        try:
+            user = SiteUser.objects.get(phoneNumber=number)
+            messages.error(request, 'Phone number is already registered!')
+            return redirect('register')
+        except:
+            pass
+        
+        if password != password2:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
 
-    gender_val = '0'
-    if gender == "Male":
-      gender_val = '1'
-    elif gender == "Female":
-      gender_val = '2'
-    elif gender == "Other":
-      gender_val = '3'
+        gender_val = '0'
+        if gender == "Male":
+            gender_val = '1'
+        elif gender == "Female":
+            gender_val = '2'
+        elif gender == "Other":
+            gender_val = '3'
 
-    user = User.objects.create_user(email.split("@")[0], '', password)
-    siteUser = SiteUser(user=user, name=name, email=email, age=age, phoneNumber=number, gender=gender_val, location=location, balance=0.0)
-    siteUser.save()
-    auth.login(request, user)
-    return redirect('main')
+        user = User.objects.create_user(email.split("@")[0], '', password)
+        siteUser = SiteUser(user=user, name=name, email=email, age=age, phoneNumber=number, gender=gender_val, location=location, balance=0.0)
+        siteUser.save()
+        auth.login(request, user)
+        return redirect('main')
 
-  return render(request, 'pages/RegisterPage.html')
+    return render(request, 'pages/RegisterPage.html')
 
 def logoutPage(request):
-  auth.logout(request)
-  return redirect('login')
+    auth.logout(request)
+    return redirect('login')
 
 @login_required(login_url='login')
 def mainPage(request):
-  return render(request, 'pages/MainPage.html')
+    return render(request, 'pages/MainPage.html')
